bee545414e46b56130e12cef5ea53b48d28ade61
Victoria-Imaobong Solomon <solomonvictoria2023@gmail.com>
2025-11-25 17:20:41 +0100

    update

--- jobs/src/Appi.jsx
+++ jobs/src/Appi.jsx
@@ -3,14 +3,30 @@ import React from 'react'
 const Appi = () => {
   return (
     <div>
+      {/*<!-- Head -->*/}
+      <section class="bg-indigo-700 py-20 mb-4">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+          <div class="text-center">
+            <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+              GetAJob!!
+            </h1>
+            <p class="my-4 text-xl text-white">
+              Find A Job With your SkillSet.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/*<!-- Developers and employers -->*/}
+      <section>
+
+      </section>
       {/*<!-- Browse Jobs -->*/}
-      <section class="bg-blue-50 px-4 py-10">  
-        <div class="container-xl 1g:container m-auto">
-            </div> 
-    </section>
-       
+      <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl 1g:container m-auto"></div>
+      </section>
     </div>
-  )
+  );
 }
 
 export default Appi
