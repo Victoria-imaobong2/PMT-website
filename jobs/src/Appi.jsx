4bc69d3bb806317ff9ac9bc85fc183dac055700b
Victoria-Imaobong Solomon <solomonvictoria2023@gmail.com>
2025-11-25 16:54:59 +0100

    update

---
+++ jobs/src/Appi.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Appi = () => {
+  return (
+    <div>
+      {/*<!-- Browse Jobs -->*/}
+      <section class="bg-blue-50 px-4 py-10">  
+        <div class="container-xl 1g:container m-auto">
+            </div> 
+    </section>
+       
+    </div>
+  )
+}
+
+export default Appi
--- jobs/src/main.jsx
+++ jobs/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Chit from './chit.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Chit />
   </StrictMode>,
 )
